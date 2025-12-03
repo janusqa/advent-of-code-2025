f7a2e2c49cbd27cbc8932d49cfb81e05424d6038
Janus Developer <dev@retrievo.net>
2025-12-03 00:33:08 -0400

    Day 2: Gift Shop

--- src/Day02/day02_a.py
+++ src/Day02/day02_a.py
@@ -1,11 +1,24 @@
 import re
 
-from utils import modulo
-
 
 class Day02A:
     def __init__(self, data: str) -> None:
         self.data = data
 
     def run(self) -> None:
-        pass
+        pattern = r"(\d{1,})-(\d{1,})"
+        matches = re.findall(pattern, self.data)
+
+        total = 0
+
+        for start, end in matches:
+            for item_id in range(int(start), int(end) + 1):
+                item_id_str = str(item_id)
+                parts_len = len(item_id_str) // 2
+                if (
+                    len(item_id_str) % 2 == 0
+                    and item_id_str[:parts_len] == item_id_str[parts_len:]
+                ):
+                    total += item_id
+
+        print(total)
--- src/Day02/day02_b.py
+++ src/Day02/day02_b.py
@@ -1,44 +1,22 @@
 import re
 
-from utils import modulo
 
-
-class Day01B:
+class Day02B:
     def __init__(self, data: str) -> None:
         self.data = data
-        self.max_num = 100
 
     def run(self) -> None:
-        pattern = r"^(R|L)(\d{1,})$"
-        matches = re.findall(pattern, self.data, re.MULTILINE)
+        pattern = r"(\d{1,})-(\d{1,})"
+        matches = re.findall(pattern, self.data)
 
-        current = 50
-        password = 0
+        total = 0
 
-        for direction, moves_str in matches:
-            moves = int(moves_str)
+        for start, end in matches:
+            for item_id in range(int(start), int(end) + 1):
+                item_id_str = str(item_id)
+                # double string, remove 1st and last char,
+                # check that original string is in  there
+                if item_id_str in (item_id_str * 2)[1:-1]:
+                    total += item_id
 
-            full_cycles = moves // self.max_num
-            partial_cycle = moves % self.max_num
-
-            match direction:
-                case "R":
-                    if (
-                        partial_cycle > 0
-                        and current != 0
-                        and current + partial_cycle >= self.max_num
-                    ):
-                        password += 1
-                    current = modulo(current + moves, self.max_num)
-                case "L":
-                    if (
-                        partial_cycle > 0
-                        and current != 0
-                        and current - partial_cycle <= 0
-                    ):
-                        password += 1
-                    current = modulo(current - moves, self.max_num)
-
-            password += full_cycles
-
-        print(password)
+        print(total)
